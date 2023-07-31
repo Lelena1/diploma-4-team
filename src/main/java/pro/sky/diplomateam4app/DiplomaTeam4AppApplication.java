a3b6cea79602558f237e3bcae8944c9ace92fb89
Lelena1 <luppol@inbox.ru>
2023-07-31 08:41:08 +0300

    Initial commit

---
+++ src/main/java/pro/sky/diplomateam4app/DiplomaTeam4AppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.diplomateam4app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaTeam4AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaTeam4AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/diplomateam4app/DiplomaTeam4AppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.diplomateam4app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaTeam4AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
